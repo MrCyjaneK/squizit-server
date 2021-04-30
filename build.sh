b03ab883eed5ffd38888a7a585534d1c2d96b62f
Czarek Nakamoto <cyjan@mrcyjanek.net>
2021-04-30 21:00:56 +0200

    pkg name....

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ checkinstall \
     --type=debian \
     --pkgversion="1.0.0+git"$(date +%Y%m%d%H%M)"."$(git log -n 1 | tr " " "\n" | head -2 | tail -1 | head -c 7)  \
     --default \
+    --pkgname="squizit-server" \
     --install=no
